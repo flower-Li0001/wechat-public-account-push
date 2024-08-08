876c10c24007fc20ddc9afd0ae2d87a5aa78ede5
flower-Li0001 <167298547+flower-Li0001@users.noreply.github.com>
2024-08-08 14:23:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -49,38 +49,7 @@ const USER_CONFIG = {
     },
   ],
 
-  USERS: [
-    {
-      // 想要发送的人的名字
-      name: '宝贝1',
-      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ovqsQ68EZN0yOdBn9jClph20LxIQ',
-      // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '24x3wdp_wmsmMoEeqm51z_F49rn9MBPcKy5LccCL4ro',
-      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '03-17',
-      festivals: [
-        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2024-06-09' },
-        // 倒计日
-        { keyword: 'birthday_message', date: '2024-08-09' },
-      ],
-    },
-  ],
+
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
