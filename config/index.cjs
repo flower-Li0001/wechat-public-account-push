b95d612ee9eec7e37747338228fc177252c9106d
flower-Li0001 <167298547+flower-Li0001@users.noreply.github.com>
2024-08-08 11:16:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -18,6 +18,28 @@ const USER_CONFIG = {
 
   USERS: [
     {
+      # 字体颜色:使用RGB颜色(如#000000黑色)，留空颜色默认随机
+"color_date":                     "",          # 日期字体颜色
+"color_region":                   "",          # 地区字体颜色
+"color_weather":                  "",          # 天气字体颜色
+"color_temp":                     "",          # 当前温度字体颜色
+"color_wind":                     "",          # 当前风向字体颜色
+"color_note_en":                  "",          # 金句英文字体颜色
+"color_note_zh":                  "",          # 金句中文字体颜色
+"color_max_temp":                 "",          # 最高温度字体颜色
+"color_min_temp":                 "",          # 最低温度字体颜色
+"color_sunrise":                  "",          # 日出字体颜色
+"color_sunset":                   "",          # 日落字体颜色
+"color_category":                 "",          # 空气质量字体颜色
+"color_pm2p5":                    "",          # pm2.5字体颜色
+"color_proposal":                 "",          # 今日建议字体颜色
+"color_chp":                      "",          # 彩虹屁字体颜色
+"color_birthday1":                "",          # 生日1字体颜色
+"color_birthday2":                "",          # 生日2字体颜色
+"color_commemoration_day1":       "",          # 纪念日1字体颜色
+"color_commemoration_day2":       "",          # 纪念日2字体颜色
+"color_yq":                       "",          # 疫情数据字体颜色
+
       // 想要发送的人的名字
       name: '小宝贝——1',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
