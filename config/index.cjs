6662a0f7618d889423bc1e167046f365487b5833
flower-Li0001 <167298547+flower-Li0001@users.noreply.github.com>
2024-08-09 15:27:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -73,7 +73,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2024-06-09' },
+        { keyword: 'love_day', date: '2024-08-08' },
         // 倒计日
         { keyword: 'birthday_message', date: '2024-12-21' },
       ],
