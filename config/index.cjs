4d4dea5c0ab21b76734e93cd645b74769bd20f6f
flower-Li0001 <167298547+flower-Li0001@users.noreply.github.com>
2024-08-13 09:45:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ovqsQ64Jk2WiV0VgHAbue9LnEZbg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '4HZfTZQFmJt_ioHDTVuIRXMeCWXWHD51j6lOISEveNc',
+      useTemplateId: 'ro1H6UCawyk4B4oOO6S_9aPd8yoQN8AHCusooQRoLQs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-17',
       festivals: [
@@ -55,7 +55,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ovqsQ68EZN0yOdBn9jClph20LxIQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '4HZfTZQFmJt_ioHDTVuIRXMeCWXWHD51j6lOISEveNc',
+      useTemplateId: 'ro1H6UCawyk4B4oOO6S_9aPd8yoQN8AHCusooQRoLQs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-17',
       festivals: [
