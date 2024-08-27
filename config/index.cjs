e299c44f023b7f548df24e925849f9297a1dd8f6
flower-Li0001 <167298547+flower-Li0001@users.noreply.github.com>
2024-08-28 07:14:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '1111ovqsQ64Jk2WiV0VgHAbue9LnEZbg',
+      id: 'ovqsQ64Jk2WiV0VgHAbue9LnEZbg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '5W-6bMl__55lTY9DVSlZsm8RE6uvq91a68Fnl0TYVrE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
